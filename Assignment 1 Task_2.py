52af26a9a942ec2625c57add50b4f13c598aa7ad
gajananp1995 <parandegajanan1995@gmail.com>
2025-05-06 12:25:15 +0530

    Add files via upload

---
+++ Assignment 1 Task_2.py
@@ -0,0 +1,9 @@
+# Taking user input for first and last name
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Concatenating first and last name to create full name
+full_name = first_name + " " + last_name  # Adding a space between names
+
+# Displaying a personalized greeting message
+print(f"Hello, {full_name}! Welcome to the Python Program.")
---
+++ Assignment 2 Task_2.py
@@ -0,0 +1,38 @@
+# Initialize sum variable
+total_sum = 0
+
+# Loop through numbers from 1 to 50
+for num in range(1, 51):  # range() includes 1 but excludes 51
+    total_sum += num  # Add the current number to total_sum
+
+# Display the final sum
+print(f"The sum of integers from 1 to 50 is: {total_sum}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
